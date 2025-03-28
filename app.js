447c827ebcb141223386906c51b2c55f500f2cf3
0111as211064.yashjain@gmail.com <0111as211064.yashjain@gmail.com>
2025-03-29 01:21:55 +0530

    Updated bs code folder with latest changes

--- app.js
+++ app.js
@@ -3,32 +3,82 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const path = require('path');
+const flash = require("connect-flash")
+const expressSession = require('express-session');
+// Add these at the top with your other requires
+const bodyParser = require('body-parser');
 
+// Add these middleware before your routes
+app.use(bodyParser.urlencoded({ extended: true })); // for form data
+app.use(bodyParser.json()); // for JSON data
+
+
+require('dotenv').config();
 // Set view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 // Static files
 app.use(express.static('public'));
 
+app.use(flash());
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+}))
+
+
 // Routes
 app.get('/', (req, res) => {
-  res.render('index');
+  const msg = req.flash("msg");
+  // console.log(msg);
+  
+  res.render('index',{msg: msg});
 });
 
 app.get('/create-room', (req, res) => {
   res.render('create-room');
 });
 
+
+
+let codes = [];
+
+app.post('/check-code',(req, res) => {
+let {roomCode} = req.body;
+// console.log(roomCode);
+
+if ( codes.indexOf(roomCode) === -1) {
+    req.flash("msg" , "Invalid room code")
+    res.redirect("/")
+    return;
+  }
+else{
+  res.redirect(`/${roomCode}`);
+  return;
+}
+
+
+})
+
 app.get('/:roomId', (req, res) => {
+  const roomId = req.params.roomId;
+  codes.push(roomId);
+  // console.log(codes);
+  
   res.render('share', { roomId: req.params.roomId });
 });
 
 // Socket.io connection
 io.on('connection', (socket) => {
+
+  
+
   const activeRooms = new Set();
 
   socket.on('join-room', (roomId) => {
+    // console.log(roomId);
+    
     const roomClients = io.sockets.adapter.rooms.get(roomId) || new Set();
     const numClients = roomClients.size;
     
@@ -58,6 +108,7 @@ io.on('connection', (socket) => {
     setTimeout(() => {
       if (activeRooms.has(roomId)) {
         io.in(roomId).socketsLeave(roomId);
+        codes.delete(roomId);
         activeRooms.delete(roomId);
       }
     }, 5000);
@@ -66,14 +117,23 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     // Clean up any rooms this socket was in
     activeRooms.forEach(roomId => {
-      io.in(roomId).socketsLeave(roomId);
-      activeRooms.delete(roomId);
+        io.in(roomId).socketsLeave(roomId);
+        activeRooms.delete(roomId);
+        
+        // Find the index of the roomId in codes array
+        const index = codes.indexOf(roomId);
+        if (index !== -1) {
+            codes.splice(index, 1); // Remove 1 element at the found index
+        }
+        // console.log(codes);
     });
-  });
 });
+});
+
+// console.log(codes);
 
 // Start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+}); 
